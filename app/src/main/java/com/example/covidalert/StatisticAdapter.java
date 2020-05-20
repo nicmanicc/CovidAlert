afa86d9d0a0e6ac556797a6c34f6d2b82bd58457
nicmanicc <nickmanic@hotmail.com>
2020-05-20 23:22:23 +1000

    Refactored to AndroidX for Google Maps API support

--- app/src/main/java/com/example/covidalert/StatisticAdapter.java
+++ app/src/main/java/com/example/covidalert/StatisticAdapter.java
@@ -1,15 +1,14 @@
 package com.example.covidalert;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;

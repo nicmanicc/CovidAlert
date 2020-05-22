d84dc2f753de8d6d264918aebaa4ccb3bcd8689b
nicmanicc <nickmanic@hotmail.com>
2020-05-23 01:17:19 +1000

    Added custom info box to country marker in maps activity

--- app/src/main/java/com/example/covidalert/StatisticAdapter.java
+++ app/src/main/java/com/example/covidalert/StatisticAdapter.java
@@ -4,15 +4,16 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
+
 
 public class StatisticAdapter extends RecyclerView.Adapter<StatisticAdapter.StatisticViewHolder> {
 
@@ -36,9 +37,11 @@ public class StatisticAdapter extends RecyclerView.Adapter<StatisticAdapter.Stat
     public class StatisticViewHolder extends RecyclerView.ViewHolder {
         public TextView countryName, confirmedView, recoveredView, deathView,
                 newConfirmed, newRecovered, newDeath;
+        public ImageButton mapButton;
 
         public StatisticViewHolder(@NonNull View itemView) {
             super(itemView);
+            mapButton = itemView.findViewById(R.id.mapButton);
             countryName = itemView.findViewById(R.id.countryName);
             confirmedView = itemView.findViewById(R.id.confirmedView);
             recoveredView = itemView.findViewById(R.id.recoveredView);
@@ -62,28 +65,47 @@ public class StatisticAdapter extends RecyclerView.Adapter<StatisticAdapter.Stat
         cases are a non-zero value.  */
     @Override
     public void onBindViewHolder(@NonNull StatisticAdapter.StatisticViewHolder statisticViewHolder, int i) {
-        //Populate the country name into the item
-        statisticViewHolder.countryName.setText(statisticList.get(i).getCountryName());
-        //Populate confirmed cases into each item
-        statisticViewHolder.confirmedView.setText(formatCases(statisticList.get(i).getConfirmedCases()));
-        statisticViewHolder.recoveredView.setText(formatCases(statisticList.get(i).getRecoveredCases()));
-        statisticViewHolder.deathView.setText(formatCases(statisticList.get(i).getDeathCases()));
-        //Retrieve new cases from statistic list
-        String newConfirmedAmount = statisticList.get(i).getNewConfirmed();
-        String newRecoveredAmount = statisticList.get(i).getNewRecovered();
-        String newDeathAmount = statisticList.get(i).getNewDeath();
+        //Retrieve individual country statistic values
+        final String countryName = statisticList.get(i).getmCountryName();
+        final String confirmedCases = statisticList.get(i).getmConfirmedCases();
+        final String recoveredCases = statisticList.get(i).getmRecoveredCases();
+        final String deathCases = statisticList.get(i).getmDeathCases();
 
-        //Check if previous days cases aren't 0
-        //Populate new cases into each item
-        if (Integer.parseInt(newConfirmedAmount) != 0) {
-            statisticViewHolder.newConfirmed.setText("+" + formatCases(newConfirmedAmount));
-        }
-        if (Integer.parseInt(newRecoveredAmount) != 0) {
-            statisticViewHolder.newRecovered.setText("+" + formatCases(newRecoveredAmount));
-        }
-        if (Integer.parseInt(newDeathAmount) != 0) {
-            statisticViewHolder.newDeath.setText("+" + formatCases(newDeathAmount));
-        }
+        //Populate the country name into the item
+        statisticViewHolder.countryName.setText(countryName);
+        //Populate confirmed cases into each item
+        statisticViewHolder.confirmedView.setText(StatisticActivity.formatCases(confirmedCases));
+        statisticViewHolder.recoveredView.setText(StatisticActivity.formatCases(recoveredCases));
+        statisticViewHolder.deathView.setText(StatisticActivity.formatCases(deathCases));
+
+        //Concatenate "+" and new cases
+        String newConfirmed = context.getResources().getString(R.string.newCases, StatisticActivity.formatCases(statisticList.get(i).getmNewConfirmed()));
+        String newRecovered = context.getResources().getString(R.string.newCases, StatisticActivity.formatCases(statisticList.get(i).getmNewRecovered()));
+        String newDeath = context.getResources().getString(R.string.newCases, StatisticActivity.formatCases(statisticList.get(i).getmNewDeath()));
+        //Set new cases
+        statisticViewHolder.newConfirmed.setText(newConfirmed);
+        statisticViewHolder.newRecovered.setText(newRecovered);
+        statisticViewHolder.newDeath.setText(newDeath);
+
+        //Get the slug (used to search in google maps api)
+        final String countrySlug = statisticList.get(i).getmCountrySlug();
+
+        //Create onclick listener for the map button and start map activity
+        statisticViewHolder.mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create new intent for the map activity
+                Intent intent = new Intent(context, MapsActivity.class);
+                //Store statistic details to display in the marker window
+                intent.putExtra("Slug", countrySlug);
+                intent.putExtra("confirmed", confirmedCases);
+                intent.putExtra("recovered", recoveredCases);
+                intent.putExtra("deaths", deathCases);
+                intent.putExtra("country", countryName);
+                //Start map activity
+                context.startActivity(intent);
+            }
+        });
     }
 
     //Get size of the statistic list
@@ -104,7 +126,7 @@ public class StatisticAdapter extends RecyclerView.Adapter<StatisticAdapter.Stat
             text = text.toLowerCase();
             //For each item in the copied list check if it contains the string
             for (com.example.covidalert.Statistic item : statisticListCopy) {
-                if (item.getCountryName().toLowerCase().contains(text)) {
+                if (item.getmCountryName().toLowerCase().contains(text)) {
                     //If item exists, add to original statitics list
                     statisticList.add(item);
                 }
@@ -112,15 +134,5 @@ public class StatisticAdapter extends RecyclerView.Adapter<StatisticAdapter.Stat
         }
         //Notify data set change and change the recycler view items
         notifyDataSetChanged();
-    }
-
-    /* Overview: Adds symbols to numbers to make them more readable.
-   The symbol will depends on what location they're in.
-   E.g. 1000 => 1,000 (for Aus) */
-    private String formatCases(String stringToFormat) {
-        //Turn the string into a number
-        Number number = Integer.parseInt(stringToFormat);
-        //Format the number using NumberFormat and return
-        return NumberFormat.getNumberInstance(Locale.getDefault()).format(number);
     }
 }

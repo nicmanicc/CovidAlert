0a069591e40452166f430230e7450276a444849c
nicmanicc <nickmanic@hotmail.com>
2020-05-31 20:41:28 +1000

    Created SafetyActivity and updated licenses

--- app/src/main/java/com/example/covidalert/MainActivity.java
+++ app/src/main/java/com/example/covidalert/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button statisticButton = findViewById(R.id.statisticsButton);
         Button informationButton = findViewById(R.id.informationButton);
+        Button safetyButton = findViewById(R.id.safetyButton);
         //Add onclick listener to start new activity
         statisticButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,9 +30,20 @@ public class MainActivity extends AppCompatActivity {
         informationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Create an intent for the InfoActivity
                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
-
+                //Start InfoActivity
                MainActivity.this.startActivity(intent);
+            }
+        });
+
+        safetyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create an intent for the SafetyActivity
+                Intent intent = new Intent(MainActivity.this, SafetyActivity.class);
+                //Start SafetyActivity
+                MainActivity.this.startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/covidalert/SafetyActivity.java
@@ -0,0 +1,14 @@
+package com.example.covidalert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SafetyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safety);
+    }
+}
